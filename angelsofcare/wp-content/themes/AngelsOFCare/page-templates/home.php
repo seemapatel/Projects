126cf4145d3c8332ceb56427d7ee21f9c943886c
seema patel <simasays@gmail.com>
2015-06-16 21:32:30 -0400

    added meta_query for upcoming_events

--- angelsofcare/wp-content/themes/AngelsOFCare/page-templates/home.php
+++ angelsofcare/wp-content/themes/AngelsOFCare/page-templates/home.php
@@ -26,10 +26,11 @@ get_header(); ?>
             'post__in' => $bloblor);
             $spotlight = new WP_Query($args); while ($spotlight->have_posts()) : $spotlight->the_post();   ?>
            <div class="sub-service">
-            <h2><?php //echo get_the_title($post->ID); ?> </h2>
-            <h2><?php the_title();?></h2>
-            <?php the_content(); ?>
-            <a href="<?php the_permalink();?>" class="read-more"><span>read more</span><i></i></a>
+                <h2><?php the_title();?></h2>
+                 <?php $content = get_the_content();  // Display the post content
+                       $content = strip_tags($content);  //strips a string from HTML ?> 
+                 <p><?php echo substr($content, 0, 200);?></p>
+                 <a href="<?php the_permalink();?>" class="read-more"><span>read more</span><i></i></a>
            </div>
             <?php endwhile; ?>
            <?php wp_reset_query(); wp_reset_postdata();?>
@@ -56,13 +57,14 @@ get_header(); ?>
                 'posts_per_page'   => 2,
                 'post_type' => 'latest_news'
             ));
- //$post_type=get_post_type(101);
-   // echo $post_type;
+ 
     while ($query->have_posts()): $query->the_post(); ?>
     <li>  <?php the_post_thumbnail() ?>
         <div class="news-text">
             <a class ="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            <p><?php the_content(); ?> <a href="<?php the_permalink(); ?>">read more</a></p>
+            <p><?php $content = get_the_content();  // Display the post content
+                     $content = strip_tags($content);  //strips a string from HTML ?> 
+            <p><?php echo substr($content, 0, 200);?> <a href="<?php the_permalink(); ?>">read more</a></p>
             <span><?php the_time(get_option('date_format')); ?></span>
         </div>
     </li>
@@ -71,36 +73,43 @@ get_header(); ?>
       <div class="upcoming-events">
           <h2>Upcoming Events</h2>
         <ul>
-           <?php
-                // get upcoming events on home page     
-               $query = null;
-               $query = new WP_Query(array(
-                           'posts_per_page'   => 3,
-                           'post_type' => 'upcoming_events'
-                       ));
+    <?php
+        /// get upcoming events on home page     
+       $query = null;
+       $query = new WP_Query(array(
+                
+                'post_type' => 'upcoming_events',  // Tell WordPress which post type we want
+                'orderby' => 'meta_value', // We want to organize the events by date
+                'meta_key' => 'date_picker', // Grab the "date picker" field created via "Advanced custom fields" plugin (stored in YYYY-MM-DD format)
+                'order' => 'ASC', // ASC is the other option    
+                'posts_per_page' => '3', // Let's show them all. 
+                'meta_query' => array( // WordPress has all the results, now, return only the events after today's date
+                    array(
+                        'key' => 'date_picker', // Check the start date field
+                        'value' => date('Y-m-d'), // Set today's date (note the similar format)
+                        'compare' => '>=', // Return the ones greater than today's date
+                        'type' => 'date', // Let WordPress know we're working with numbers
+                        ),
+                    ),
+               ));
 
             while ($query->have_posts()): $query->the_post(); ?>
             <li>  
-         <?php  $format_in = 'Ymd'; // the format your value is saved in (set in the field options)
+         <?php  
+                $format_in = 'Ymd'; // the format your value is saved in (set in the field options)
                 $format_out = 'M-d-Y'; // the format you want to end up with
                 $date = DateTime::createFromFormat($format_in, get_field('date_picker',$post->ID));
-                $date2=$date->format('d/m/y');
-                //$date1=date('d/m/y');
-                print_r($tempArr);
-              //  $date2 = date("d/m/y", mktime(0, 0, 0, $tempArr[1], $tempArr[0], $tempArr[2]));
-                echo  $post->ID.' '.$date1.' '.$date2.'<br/>';
-                
-                if(strtotime($date2)>=strtotime($date1)){
-                    echo "hi";
-                    $format_date=explode('-',$date->format( $format_out )); ?>
-
+                // how to displlay the date need to ask anurag ?  
+                $format_date=explode('-',$date->format( $format_out )); ?>
                 <div class="date-part"> <span class="month"><?php echo $format_date[0]; ?></span> <span class="date"><?php echo $format_date[1];?></span> </div>
                 <div class="news-text"> 
                     <a class ="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    <p><?php the_content(); ?> <a href="<?php the_permalink(); ?>">read more</a></p>
+                    <p><?php $content = get_the_content();  // Display the post content
+                             $content = strip_tags($content);  //strips a string from HTML ?> 
+                    <p><?php echo substr($content, 0, 200);?> <a href="<?php the_permalink(); ?>">read more</a></p>
                 </div>
             </li>
-                <?php } endwhile;?></ul>
+                <?php  endwhile;?></ul>
         <a href="<?php echo get_option('home'); ?> /upcoming-events" class="read-more"><span>see more</span><i></i></a>
      </div><!--upcoming events-->
   
